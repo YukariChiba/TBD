c465399f972df58929381c7a0cc9bfb7d3b73607
Charles <Charles@0x7f.cc>
2022-01-07 07:29:29 +0800

    Fix bug in github action

--- TBD.sh
+++ TBD.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 function build_package {
+	echo "Building file [$1]..."
 	source $1
-	docker run -it -v $(realpath $1):/src/pkgmeta -v $(dirname "$(realpath "${BASH_SOURCE[0]}")")/tTBD.sh:/usr/bin/tTBD.sh -v $PWD:/publish $image "bash" "-c" "source /src/pkgmeta && source /usr/bin/tTBD.sh"
+	docker run -i -v $(realpath $1):/src/pkgmeta -v $(dirname "$(realpath "${BASH_SOURCE[0]}")")/tTBD.sh:/usr/bin/tTBD.sh -v $PWD:/publish $image "bash" "-c" "source /src/pkgmeta && source /usr/bin/tTBD.sh"
 }
 
 if [[ $# -eq 0 ]] ; then

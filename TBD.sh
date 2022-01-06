24f115870659086c0aa9f033781a2965c77202f1
Charles <Charles@0x7f.cc>
2022-01-07 07:23:29 +0800

    Add gitignore, add hello-world example, add build action workflow, modify TBD.sh

--- TBD.sh
+++ TBD.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-source $1
-docker run -it -v $(realpath $1):/src/pkgmeta -v $(dirname "$(realpath "${BASH_SOURCE[0]}")")/tTBD.sh:/usr/bin/tTBD.sh -v $PWD:/publish $image "bash" "-c" "source /src/pkgmeta && source /usr/bin/tTBD.sh"
+
+function build_package {
+	source $1
+	docker run -it -v $(realpath $1):/src/pkgmeta -v $(dirname "$(realpath "${BASH_SOURCE[0]}")")/tTBD.sh:/usr/bin/tTBD.sh -v $PWD:/publish $image "bash" "-c" "source /src/pkgmeta && source /usr/bin/tTBD.sh"
+}
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Please specify a filename or `all` for all .TBD files.'
+    exit 0
+fi
+
+if [ $1 = "all" ]; then
+   for tbdfil in *.TBD; do
+	   build_package $tbdfile
+   done
+else
+   build_package $1
+fi
